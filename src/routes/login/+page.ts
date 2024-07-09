e18fcf4d5c2b85645f5d22135adb0dcd6f914f7d
Joey Remmers <joeyjoeyremmers@gmail.com>
2024-07-09 13:11:32 +0200

    got the authtoken! but still get errors

--- src/routes/login/+page.ts
+++ src/routes/login/+page.ts
@@ -4,13 +4,14 @@ import { authToken } from "$lib";
 import { GetApiToken } from '../../stores/AuthStore';
 
 export const load = (async () => {
-    page.subscribe(async (page) => {
-        const userMail = page.data.session?.user?.email
+    // page.subscribe(async (page) => {
+    //     const userMail = page.data.session?.user?.email
 
-        if  (userMail != undefined)
-        {
-            authToken.set( await GetApiToken(userMail) )
-        }
-    })
+    //     if  (userMail != undefined)
+    //     {
+    //         authToken.set( await GetApiToken(userMail) )
+    //     }
+    // })
+    await authToken.set( await GetApiToken("joeyjoeyremmers@gmail.com") )
     return {};
 }) satisfies PageLoad;
--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,17 +1,23 @@
 export async function GetApiToken(UserMail: string): Promise<string> {
     console.log("GetApiToken " + UserMail);
 
-    var response = ((await fetch(`${import.meta.env.VITE_API_URL}/login`, {
+    return fetch(`${import.meta.env.VITE_API_URL}/login`, {
         method: 'POST',
         headers: {
-            Accept: 'application/json',
-          },
-        body: UserMail
-    })));
-
-    if (!response.ok)
-    {
-        console.log(response.statusText);
-    }
-    return await response.text();
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(UserMail)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(parsedData => {
+        return parsedData
+    })
+    .catch(error => {
+        console.error('Error sending data:', error);
+    });
 }
